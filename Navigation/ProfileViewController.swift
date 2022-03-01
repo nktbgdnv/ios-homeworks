2be815798f21be57af9965ad7bba71c6a4dd747a
Nikita Bogdanov <nktbgdnv@yandex.com>
2022-03-01 23:05:38 +0300

    Add for feature

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Никита Богданов on 27.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
