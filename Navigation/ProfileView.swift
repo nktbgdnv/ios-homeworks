4625192ab0eed781d5a5f747a7c3f820c5599ce6
Nikita Bogdanov <nktbgdnv@yandex.com>
2022-03-14 22:46:32 +0300

    New Add for feature-3

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Никита Богданов on 14.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
